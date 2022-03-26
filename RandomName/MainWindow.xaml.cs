7cecd40ee12cf30d2cc8b1ed9d21201ed052b42e
Harris Wilde <Harris Wilde@DESKTOP-JDNO02O>
2022-03-26 14:16:11 +0800

    添加项目文件。

---
+++ RandomName/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace RandomName
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RandomNameSelector selector;
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> list = new List<string>() { "赵12","钱45","孙13", "李23"};
+            selector = new RandomNameSelector();
+            selector.Selecting += Selector_Selecting;
+            selector.Selected += Selector_Selected;
+            this.ICCandidateNames.ItemsSource = selector._Namelist;
+            this.ProgressBarForLeft.Maximum = selector._Namelist.Count;
+            this.ProgressBarForLeft.Value = selector._Namelist.Count;
+        }
+
+        private void Selector_Selected(object sender, NameEventArgs e)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                this.ICSelectedNames.Items.Add($"{ICSelectedNames.Items.Count + 1}. {e.Name}");
+                selector._Namelist.Remove(this.TextBlockSelectedName.Text);
+                this.ProgressBarForLeft.Value = selector._Namelist.Count;
+            }));
+        }
+
+        private void Selector_Selecting(object sender, NameEventArgs e)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                this.TextBlockSelectedName.Text = e.Name;
+            }));
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private async void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            ICSelectedNames.Items.Clear();
+            if (selector._Namelist.Count != 0)
+            {
+                if (this.ToggleButtonIsMulti.IsChecked == true)
+                {
+                    if (selector._Namelist.Count < int.Parse(TextBoxPeopleNum.Text))
+                    {
+                        TextBoxPeopleNum.Text = $"{selector._Namelist.Count}";
+                    }
+                    this.ButtonStart.IsEnabled = false;
+                    await selector.StartSelectingWithTime(int.Parse(this.TextBoxPeopleNum.Text));
+                    this.ButtonStart.IsEnabled = true;
+                } else
+                {
+                    if (this.ButtonStart.Content.Equals("开始"))
+                    {
+                        this.ButtonStart.Content = "停止";
+                        await selector.StartSelecting();
+                    } else
+                    {
+                        this.ButtonStart.Content = "开始";
+                        selector.IsSelecting = false;
+                    }
+                }
+            }
+        }
+
+        private void ButtonReset_Click(object sender, RoutedEventArgs e)
+        {
+            selector.ResetNameList();
+            this.ProgressBarForLeft.Value = this.ProgressBarForLeft.Maximum;
+        }
+
+        private void RButtonNumAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.Parse(TextBoxPeopleNum.Text) < selector._Namelist.Count)
+            {
+                TextBoxPeopleNum.Text = $"{int.Parse(TextBoxPeopleNum.Text) + 1}";
+            }
+        }
+
+        private void RButtonNumSub_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.Parse(TextBoxPeopleNum.Text) > 1)
+            {
+                TextBoxPeopleNum.Text = $"{int.Parse(TextBoxPeopleNum.Text) - 1}";
+            }
+        }
+
+        private void ButtonCopy_Click(object sender, RoutedEventArgs e)
+        {
+            this.ButtonCopy.Content = "已复制";
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 3000;
+            timer.AutoReset = false;
+            timer.Elapsed += (s, e2) =>this.Dispatcher.Invoke(() => this.ButtonCopy.Content = "复制");
+            timer.Start();
+            string copyText = "";
+            foreach (var item in ICSelectedNames.Items)
+            {
+                copyText += $"{item.ToString()}\n";
+            }
+
+            Clipboard.SetText(copyText);
+        }
+    }
+}
---
+++ RandomName/RandomNameSelector.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Threading;
+
+namespace RandomName
+{
+    public class NameEventArgs: EventArgs
+    {
+        public string Name { get; set; }
+    }
+
+    public delegate void NameEventHandler(object sender, NameEventArgs e);
+    public class RandomNameSelector
+    {
+        private NameEventHandler SelectingNameEventHandler;
+        private NameEventHandler SelectedNameEventHandler;
+
+        public event NameEventHandler Selecting
+        {
+            add { this.SelectingNameEventHandler += value; }
+            remove { this.SelectingNameEventHandler -= value; }
+        }
+
+        public event NameEventHandler Selected
+        {
+            add { this.SelectedNameEventHandler += value; }
+            remove { this.SelectedNameEventHandler -= value; }
+        }
+
+        private List<string> _List;
+        public BindingList<string> _Namelist;
+        public bool IsSelecting;
+
+        public RandomNameSelector()
+        {
+            List<string> list = new List<string>();
+            if (!System.IO.File.Exists("class.txt"))
+            {
+                System.IO.StreamWriter streamWriter = new System.IO.StreamWriter("class.txt", true, new System.Text.UTF8Encoding(false));
+                streamWriter.Close();
+            } else
+            {
+                System.IO.StreamReader streamReader = new System.IO.StreamReader("class.txt",true);
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    if (line.Length > 0)
+                    {
+                        list.Add(line);
+                    }
+                }
+            }
+
+            _List = list;
+            _Namelist = new BindingList<string>();
+            _List.ForEach(x => _Namelist.Add(x));
+            IsSelecting = false;
+        }
+
+        public void ResetNameList()
+        {
+            _Namelist.Clear();
+            _List.ForEach(x => _Namelist.Add(x));
+        }
+
+        public async Task StartSelecting()
+        {
+            IsSelecting = true;
+            await Task.Run(() =>
+            {
+                Random random = new Random();
+                int index = 0;
+                while (IsSelecting)
+                {
+                    index = random.Next(0, _Namelist.Count);
+                    if (SelectingNameEventHandler != null)
+                    {
+                        SelectingNameEventHandler.Invoke(this, new NameEventArgs { Name = _Namelist[index] });
+                    }
+                    Thread.Sleep(50);
+                }
+
+                SelectedNameEventHandler.Invoke(this, new NameEventArgs { Name = _Namelist[index] });
+            });
+            
+        }
+
+        public async Task StartSelectingWithTime(int times)
+        {
+            await Task.Run(() =>
+            {
+                Random random = new Random();
+                int index = 0;
+                for (int i = 0; i < times; i++)
+                {
+                    index = random.Next(0, _Namelist.Count);
+                    if (SelectingNameEventHandler != null)
+                    {
+                        SelectingNameEventHandler.Invoke(this, new NameEventArgs { Name = _Namelist[index] });
+                        SelectedNameEventHandler.Invoke(this, new NameEventArgs { Name = _Namelist[index] });
+                    }
+                    Thread.Sleep(100);
+                }
+            });
+
+        }
+    }
+}
